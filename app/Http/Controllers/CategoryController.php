c8930c31adcc61a7e714fdc86729a4fcda4239e7
Aldo Gallegos <pedroaldo14@gmail.com>
2023-08-29 19:56:21 -0500

    Implement CRUD

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function getCategories()
+    {
+        return response()->json(Category::all(), 200);
+    }
+
+    public function getCategory($id)
+    {
+        $category = Category::find($id);
+
+        if (is_null($category)) {
+            return response()->json(['message' => 'Registro no encontrado.'], 404);
+        }
+
+        return response()->json($category, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $category = Category::create($request->all());
+
+        if (is_null($category)) {
+            return response()->json(['message' => 'Ocurrió un error inesperado al guardar la categoría.'], 404);
+        }
+
+        return response()->json($category, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+
+        if (is_null($category)) {
+            return response()->json(['message' => 'Registro no encontrado.'], 404);
+        }
+
+        $category->update($request->all());
+
+        return response()->json($category, 200);
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+
+        if (is_null($category)) {
+            return response()->json(['message' => 'Registro no encontrado.'], 404);
+        }
+
+        $category->delete();
+
+        return response()->json(['message' => 'Registro eliminado correctamente.'], 200);
+    }
+}

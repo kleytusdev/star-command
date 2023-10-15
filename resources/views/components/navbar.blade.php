e94547128e202e2e49251480f0ba84c1f5ae79f5
Aldo Gallegos <pedroaldo14@gmail.com>
2023-10-15 17:01:02 -0500

    dark mode util

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,37 +8,15 @@
     <div class="flex items-center gap-3">
         <x-button
             type="button"
-            class="md:hidden"
-            icon-only
-            variant="secondary"
-            sr-text="Toggle dark mode"
-            x-on:click="toggleTheme"
-        >
-            <x-heroicon-o-moon
-                x-show="!isDarkMode"
-                aria-hidden="true"
-                class="w-6 h-6"
-            />
-
-            <x-heroicon-o-sun
-                x-show="isDarkMode"
-                aria-hidden="true"
-                class="w-6 h-6"
-            />
-        </x-button>
-    </div>
-
-    <div class="flex items-center gap-3">
-        <x-button
-            type="button"
             class="hidden md:inline-flex"
             icon-only
             variant="secondary"
             sr-text="Toggle dark mode"
             x-on:click="toggleTheme"
+            @click="isDarkMode = ! isDarkMode"
         >
             <x-heroicon-o-moon
-                x-show="!isDarkMode"
+                x-show="! isDarkMode"
                 aria-hidden="true"
                 class="w-6 h-6"
             />
@@ -97,47 +75,3 @@
         </x-dropdown>
     </div>
 </nav>
-
-{{-- <!-- Mobile bottom bar -->
-<div
-    class="fixed inset-x-0 bottom-0 flex items-center justify-between px-4 py-4 sm:px-6 transition-transform duration-500 bg-white md:hidden dark:bg-dark-eval-1"
-    :class="{
-        'translate-y-full': scrollingDown,
-        'translate-y-0': scrollingUp,
-    }"
->
-    <x-button
-        type="button"
-        icon-only
-        variant="secondary"
-        sr-text="Search"
-    >
-        <x-heroicon-o-search aria-hidden="true" class="w-6 h-6" />
-    </x-button>
-
-    <a href="{{ route('dashboard') }}">
-        <x-application-logo aria-hidden="true" class="w-10 h-10" />
-
-        <span class="sr-only">Dashboard</span>
-    </a>
-
-    <x-button
-        type="button"
-        icon-only
-        variant="secondary"
-        sr-text="Open main menu"
-        x-on:click="isSidebarOpen = !isSidebarOpen"
-    >
-        <x-heroicon-o-menu
-            x-show="!isSidebarOpen"
-            aria-hidden="true"
-            class="w-6 h-6"
-        />
-
-        <x-heroicon-o-x
-            x-show="isSidebarOpen"
-            aria-hidden="true"
-            class="w-6 h-6"
-        />
-    </x-button>
-</div> --}}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    x-data="{ isDarkMode: localStorage.getItem('dark') === 'true' }"
+    x-init="$watch('isDarkMode', val => localStorage.setItem('dark', val))"
+    x-bind:class="{ 'dark': isDarkMode }">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'K UI') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Fonts -->
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet" />
-
-    {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <script defer src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script> --}}
-
 
     <!-- Styles -->
     <style>
@@ -28,22 +27,16 @@
 </head>
 
 <body class="font-nunito antialiased">
-    <div
-        x-data="mainState"
-        :class="{ dark: isDarkMode }"
-        x-on:resize.window="handleWindowResize"
-        x-cloak
-    >
-        <div class="min-h-screen text-gray-900 duration-500 bg-gray-100 dark:bg-dark-eval-0 dark:text-gray-200 flex flex-row">
+    <div x-on:resize.window="handleWindowResize" x-cloak>
+        <div
+            class="min-h-screen text-gray-900 duration-500 bg-gray-100 dark:bg-dark-eval-0 dark:text-gray-200 flex flex-row">
             <!-- Sidebar -->
             <div class="flex flex-2">
                 <x-sidebar.sidebar />
             </div>
 
             <!-- Page Wrapper -->
-            <div
-                class="flex flex-1 flex-col min-h-screen"
-            >
+            <div class="flex flex-1 flex-col min-h-screen">
                 <!-- Page Content -->
                 <main class="py-4 px-4 sm:px-6 flex-1">
                     {{ $slot }}
@@ -52,4 +45,5 @@
         </div>
     </div>
 </body>
+
 </html>

922829b6b5758b1768a6ba3374993c4f8673c805
Aldo Gallegos <pedroaldo14@gmail.com>
2023-11-15 00:40:22 -0500

    styles show product

--- resources/views/livewire/product/show.blade.php
+++ resources/views/livewire/product/show.blade.php
@@ -1,29 +1,27 @@
-<div class="mx-auto my-auto flex max-w-xs flex-col items-center rounded-xl border px-4 py-4 text-center md:max-w-lg md:flex-row md:items-start md:text-left">
-    <div class="mb-4 md:mr-6 md:mb-0">
-      <img class="h-56 rounded-lg object-cover md:w-56" src="{{ asset('storage/products/' . $product->photo_uri) }}" alt="" />
+<div class="flex flex-1 flex-col h-[100%]">
+    <!-- Navbar -->
+    <x-navbar title="Productos" />
+    <div class="flex-1 p-6 bg-white rounded-xl dark:bg-dark-eval-0 overflow-y-auto">
+        <div
+            class="mx-auto my-auto flex max-w-xs flex-col items-center rounded-xl border-[1px] border-gray-200 dark:border-dark-eval-2 px-4 py-4 text-center md:max-w-lg md:flex-row md:items-center md:text-left">
+            <div class="md:mr-6 md:mb-0">
+                <img class="h-56 rounded-lg object-cover md:w-56"
+                    src="{{ asset('storage/products/' . $product->photo_uri) }}" alt="" />
+            </div>
+            <div class="">
+                <p class="text-xl font-semibold text-gray-700 dark:text-white">{{ $product->name }}</p>
+                <p class="mb-4 text-sm font-medium text-gray-500 dark:text-gray-200">{{ $product->category->name }}</p>
+                <p class="mb-4 text-sm font-medium text-gray-500 dark:text-gray-200">{{ $product->model }}</p>
+                <p class="mb-4 text-sm font-medium text-gray-500 dark:text-gray-200">{{ $product->brand }}</p>
+                <p class="text-sm font-medium text-gray-700 dark:text-gray-200">Stock disponible: {{ $product->stock }}</p>
+                <div class="flex items-center mt-4">
+                    @if ($product->status->value === 'Activo')
+                        <div class="h-2.5 w-2.5 rounded-full bg-p-green mr-2"></div> <p class="text-sm font-bold text-gray-700 dark:text-gray-200">{{ $product->status }}</p>
+                    @else
+                        <div class="h-2.5 w-2.5 rounded-full bg-p-red mr-2"></div> <p class="text-sm font-bold text-gray-700 dark:text-gray-200">{{ $product->status }}</p>
+                    @endif
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="">
-      <p class="text-xl font-medium text-gray-700">{{ $product->name }}</p>
-      <p class="mb-4 text-sm font-medium text-gray-500">Senior Editor</p>
-      <div class="flex space-x-2">
-        <div class="flex flex-col items-center rounded-xl bg-gray-100 px-4 py-2">
-          <p class="text-sm font-medium text-gray-500">Articles</p>
-          <p class="text-3xl font-medium text-gray-600">13</p>
-        </div>
-        <div class="flex flex-col items-center rounded-xl bg-gray-100 px-4 py-2">
-          <p class="text-sm font-medium text-gray-500">Papers</p>
-          <p class="text-3xl font-medium text-gray-600">7</p>
-        </div>
-        <div class="flex flex-col items-center rounded-xl bg-gray-100 px-4 py-2">
-          <p class="text-sm font-medium text-gray-500">Followers</p>
-          <p class="text-3xl font-medium text-gray-600">2.5k</p>
-        </div>
-        <div class=""></div>
-      </div>
-      <div class="mb-3"></div>
-      <div class="flex space-x-2">
-        <button class="w-full rounded-lg border-2 bg-white px-4 py-2 font-medium text-gray-500">Message</button>
-        <button class="w-full rounded-lg border-2 border-transparent bg-blue-600 px-4 py-2 font-medium text-white">Follow</button>
-      </div>
-    </div>
-  </div>
+</div>

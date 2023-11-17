a11d311416ae24447ab80d643f3e205a220b50fa
Aldo Gallegos <pedroaldo14@gmail.com>
2023-11-16 20:13:39 -0500

    first build

--- app/Livewire/Subuser/Table.php
+++ app/Livewire/Subuser/Table.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Subuser;
 
 use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Table extends Component
 {
@@ -15,23 +16,24 @@ class Table extends Component
     public function getSubusers()
     {
         return User::has('roles')->has('person')
-            ->with(['roles', 'person'])
-            ->get()
-            ->map(function ($user) {
-                return (object) [
-                    'id' => $user->id,
-                    'role' => $user->roles[0]['name'],
-                    'photoUri' => $user->photo_uri,
-                    'email' => $user->email,
-                    'name' => ucwords(strtolower($user->person->name)),
-                    'documentNumber' => $user->person->document_number,
-                    'phoneNumber' => $user->person->phone_number,
-                    'paternalSurname' => $user->person->paternal_surname,
-                    'maternalSurname' => $user->person->maternal_surname,
-                    'active' => $user->active ? 'Activo' : 'Deshabilitado',
-                ];
-            })
-            ->sortByDesc('created_at')
-            ->values();
+        ->where('id', '!=', Auth::id()) // Excluir usuario autenticado
+        ->with(['roles', 'person'])
+        ->orderByDesc('created_at') // Ordenar descendientemente por created_at
+        ->get()
+        ->map(function ($user) {
+            return (object) [
+                'id' => $user->id,
+                'role' => $user->roles[0]['name'],
+                'photoUri' => $user->photo_uri,
+                'email' => $user->email,
+                'name' => ucwords(strtolower($user->person->name)),
+                'documentNumber' => $user->person->document_number,
+                'phoneNumber' => $user->person->phone_number,
+                'paternalSurname' => $user->person->paternal_surname,
+                'maternalSurname' => $user->person->maternal_surname,
+                'active' => $user->active ? 'Activo' : 'Deshabilitado',
+            ];
+        })
+        ->values();
     }
 }
